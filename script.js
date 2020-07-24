e78e5c5f9bb2b6cde407c549006c20dc1903d633
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-07-25 00:59:42 +0530

    shorten

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 var toggle = document.querySelector('input[name="toggle"]');
 var socialImg = document.querySelector("#social_img");
+var themeStyle = document.getElementById("theme-style");
 
+const darkThemeFile = "blue.css";
+const lightThemeFile = "default.css";
 const darkImgUrl =
   "https://github-readme-stats.vercel.app/api?username=kk-007&&show_icons=true&title_color=eeeeee&icon_color=fff&text_color=eeeeee&bg_color=164d56";
-
 const lightImgUrl =
   "https://github-readme-stats.vercel.app/api?username=kk-007&&show_icons=true&title_color=000000&icon_color=000000&text_color=000000&bg_color=eaeaea";
 
 const changeTheme = () => {
-  if (toggle.checked) {
-    document.getElementById("theme-style").href = "blue.css";
-    socialImg.src = darkImgUrl;
-  } else {
-    document.getElementById("theme-style").href = "default.css";
-    socialImg.src = lightImgUrl;
-  }
+  themeStyle.href = toggle.checked ? darkThemeFile : lightThemeFile;
+  socialImg.src = toggle.checked ? darkImgUrl : lightImgUrl;
 };
-document.getElementById("theme-style").href = "blue.css";
+themeStyle.href = darkThemeFile;
 socialImg.src = darkImgUrl;
