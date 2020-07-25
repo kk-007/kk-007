ca929f231348788086de0411e5a958e3535a7a64
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-07-25 19:46:19 +0530

    make dark theme as default theme

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var toggle = document.querySelector('input[name="toggle"]');
 var socialImg = document.querySelector("#social_img");
 var themeStyle = document.getElementById("theme-style");
 
-const darkThemeFile = "blue.css";
-const lightThemeFile = "default.css";
+const darkThemeFile = "default.css";
+const lightThemeFile = "light.css";
 const darkImgUrl =
   "https://github-readme-stats.vercel.app/api?username=kk-007&&show_icons=true&title_color=eeeeee&icon_color=fff&text_color=eeeeee&bg_color=164d56";
 const lightImgUrl =
@@ -13,5 +13,3 @@ const changeTheme = () => {
   themeStyle.href = toggle.checked ? darkThemeFile : lightThemeFile;
   socialImg.src = toggle.checked ? darkImgUrl : lightImgUrl;
 };
-themeStyle.href = darkThemeFile;
-socialImg.src = darkImgUrl;
